b908b4c774402186d94800d3d199acf58e9bf544
Veli Kara <113050906+velikkom@users.noreply.github.com>
2024-10-17 21:32:33 +0300

    Çatışmalar çözüldü

--- src/components/common/header/topbar.jsx
+++ src/components/common/header/topbar.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { config } from '@/helpers/config'
+import { Container } from 'react-bootstrap'
 
 export const Topbar = () => {
   return (
-    <div>Topbar</div>
+    <div>
+      <Container className="topbar">
+        <div className="slogan">
+          <i className= "pipi-megaphone"></i>
+          {config.project.slogan}
+        </div>
+      </Container>
+    </div>
   )
 }
